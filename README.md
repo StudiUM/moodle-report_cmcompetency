8ae7351c598d7efe5f61b40cc80c3391a94ecad0
Issam Taboubi <issam.taboubi@umontreal.ca>
2019-02-12 10:36:11 -0500

    EVOSTDM-1586 - APC - Éval-comp-activité : Structure de plugin report_cmcompetency

---
+++ README.md
@@ -0,0 +1,2 @@
+# Description
+Reports based on the course module competency to offer students and teachers a better user experience.
---
+++ classes/api.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class for doing things related to course module competencies report.
+ *
+ * @package    report_cmcompetency
+ * @author     Issam Taboubi <issam.taboubi@umontreal.ca>
+ * @copyright  2019 Université de Montréal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace report_cmcompetency;
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class for doing things with report cmcompetency.
+ *
+ * @author     Issam Taboubi <issam.taboubi@umontreal.ca>
+ * @copyright  2019 Université de Montréal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class api {
+
+}
